0772939796f242802035e112c7051a63b7f4015f
sujalbindra1012 <sujalbindra@gmail.com>
2024-07-25 14:01:14 +0530

    components created

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Home.css';
 import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 const Home = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <ExploreMenu />
+        </div>
+    )
 }
 
 export default Home
