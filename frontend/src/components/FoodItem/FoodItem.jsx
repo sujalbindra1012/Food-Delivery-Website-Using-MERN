a3ec136f30ae103dc6714333a043c59b556e542a
sujalbindra1012 <sujalbindra@gmail.com>
2024-07-30 17:14:05 +0530

    data fetched from database

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -6,11 +6,14 @@ import { StoreContext } from '../../context/StoreContext';
 const FoodItem = ({ id, name, price, description, image }) => {
 
     const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+    const url = "http://localhost:4000";
+    
 
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
-                <img className='food-item-image' src={image} alt="" />
+                <img className='food-item-image' src={url + "/images/" + image} alt="" />
+
                 {
                     !cartItems[id]
                         ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} />
